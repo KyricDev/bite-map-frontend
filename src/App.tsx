497289294c2efdfa97a99303c76f00aaa4200232
Ricky Dave Madrid <madridrickydave@gmail.com>
2025-05-05 06:29:33 +0800

    removed boiler plate. Added basic blurb

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import './index.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App()
+{
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='flex flex-column flex-center'>
+      <h1>BITE <span className='font-color-pink'>MAP</span></h1>
+      <h3>Find Your Perfect Dining Spot</h3>
+      <p>Discover the best restaurants near you with our easy-to-use restaurant finder tool. Whether you're craving a cozy cafe, a trendy bistro, or a fine dining experience, we've got you covered.</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
