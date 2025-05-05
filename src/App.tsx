ba1755f298dfd8f53a112693fda9760e68f44906
Ricky Dave Madrid <madridrickydave@gmail.com>
2025-05-05 16:27:35 +0800

    added checkbox for using location

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
 import './App.css';
 import './index.css';
-import { Button, TextField, ThemeProvider, createTheme } from '@mui/material';
+import { Button, Checkbox, IconButton, TextField, ThemeProvider, Tooltip, createTheme } from '@mui/material';
+import InfoIcon from '@mui/icons-material/Info';
+import { useState } from 'react';
 
 const theme = createTheme({
   palette: {
@@ -13,11 +14,12 @@ const theme = createTheme({
 
 function App()
 {
+  const [canUseLocation, setCanUseLocation] = useState(true);
 
   return (
     <ThemeProvider theme={theme}>
       <div className='flex flex-column flex-center'>
-        <h1>BITE <span className='font-color-pink'>MAP</span></h1>
+        <h1>BITE<span className='font-color-pink'>MAP</span></h1>
         <h3>Find Your Perfect Dining Spot</h3>
         <p>Discover the best restaurants near you with our easy-to-use restaurant finder tool. Whether you're craving a cozy cafe, a trendy bistro, or a fine dining experience, we've got you covered.</p>
         <div className='flex flex-center'>
@@ -29,7 +31,41 @@ function App()
           <div style={{
             width: '20px'
           }}></div>
-          <Button variant='contained' size='large'>Ask</Button>
+          <Button variant='contained' size='large' onClick={() =>
+          {
+            if (canUseLocation) {
+              const geolocation = navigator.geolocation;
+
+              geolocation.getCurrentPosition((position) =>
+              {
+                const coordinates = position.coords;
+                const lat = coordinates.latitude;
+                const long = coordinates.longitude;
+              }, (error) =>
+              {
+                
+              });
+
+              return;
+            }
+
+
+          }}>FIND</Button>
+        </div>
+        <div className='flex flex-center'>
+          <Checkbox
+            checked={canUseLocation}
+            onClick={() => setCanUseLocation((value) => value ? false : true)}
+          />
+          <div>
+            Use location
+          </div>
+          <div style={{
+            width: '5px'
+          }}></div>
+          <Tooltip title='Using your current location allows better responses'>
+            <InfoIcon />
+          </Tooltip>
         </div>
       </div>
     </ThemeProvider>
