c2113db4c2d52fc15541dfa0ec5d8a9cbe642876
Ricky Dave Madrid <madridrickydave@gmail.com>
2025-05-08 00:18:28 +0800

    added setting of api url based on localhost

--- src/constants/api-uris.ts
+++ src/constants/api-uris.ts
@@ -1,4 +1,4 @@
 // const apiUri = 'http://localhost:3000/api';
-const apiUri = 'https://bite-map-backend.onrender.com/api';
+const apiUri =  window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'https://bite-map-backend.onrender.com/api';
 export const searchUri = `${apiUri}/execute`;
 export const testUri = `${apiUri}/test`;
