ab2bdaa9c088549cfe2df1cabac964509b7bb9fd
Ricky Dave Madrid <madridrickydave@gmail.com>
2025-05-07 18:01:29 +0800

    changed api uri to use https://bite-map-backend.onrender.com/api

--- src/constants/api-uris.ts
+++ src/constants/api-uris.ts
@@ -1,3 +1,4 @@
-const apiUri = 'http://localhost:3000/api';
+// const apiUri = 'http://localhost:3000/api';
+const apiUri = 'https://bite-map-backend.onrender.com/api';
 export const searchUri = `${apiUri}/execute`;
 export const testUri = `${apiUri}/test`;
